0e8916032722ab83175602650944490eefe7a890
PTBYSR <paul.emechebe@gmail.com>
2022-09-12 19:08:28 +0100

    update banner

--- components/landingpage/Banner.tsx
+++ components/landingpage/Banner.tsx
@@ -6,7 +6,7 @@ import trianglesred from '../../public/trianglesred.svg'
 function Banner() {
   return (
     <section id='banner' className='bg-[#66cee8]'>
-        <div className="container relative flex flex-col py-28 px-4 mx-auto mt-10 space-y-12 md:space-y-0 md:flex-row ">
+        <div className="container relative flex flex-col py-28 px-4 mx-auto  space-y-12 md:space-y-0 md:flex-row ">
             <div className="flex flex-col space-y-12 md:w-1/2">
                 <h2 className="text-white max-w-lg text-4xl md:text-5xl font-bold text-center md:text-left">
                     Kickstart your developer journey now!
@@ -20,9 +20,9 @@ function Banner() {
                     <a href='' className='p-4 px-8 font-regular text-m text-black bg-white baseline hover:scale-105 transition'>Start now!</a>
                 </Link>
             </div>
-        <div className="absolute md:-top-16 md:w-[500px] w-[400px] -top-24 left-0">
+        {/* <div className="absolute md:-top-16 md:w-[500px] w-[400px] -top-24 left-0">
             <Image src={trianglesred} />
-        </div>
+        </div> */}
         </div>
     </section>
     )
