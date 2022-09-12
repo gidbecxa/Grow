e668be1af095b2d98250de41c53320521a71f5b6
PTBYSR <paul.emechebe@gmail.com>
2022-09-12 18:01:02 +0100

    update community.tsx

--- components/landingpage/Community.tsx
+++ components/landingpage/Community.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from 'next/link'
 import communitysection from "../../public/communitysection.jpg";
 import triangles from "../../public/triangles.svg";
 import {
@@ -21,7 +22,7 @@ const Community = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <>
-      <section id="community" className="bg-[66cee8]">
+      <section id="community" className="bg-[#448a9c]">
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
@@ -80,19 +81,22 @@ const Community = () => {
           <div className="container relative flex flex-col py-28 px-4 mx-auto mt-10 space-y-12 md:space-y-0 md:flex-row ">
             <div className="flex flex-col space-y-12 md:w-1/2">
                 <h2 className="text-white max-w-lg text-4xl md:text-5xl font-bold text-center md:text-left">
-                    Kickstart your developer journey now!
+                Join the Grow Community
                 </h2>
                 <p className="text-white text-lg md:text-xl font-medium text-center md:text-left">
-                    Let us help you get started in 6 weeks! Join our entry-level developer bootcamp, and you are good to go!
+                Grow is the leading software developement school and community for
+              French-speaking Africans. We are commited to helping millions of
+              francophones with little to no coding experience become world-class
+              certified software engineers.
                 </p>
             </div>
             <div className="flex justify-center items-center text-center space-y-12 md:w-1/2">
-                <Link href='/programs' passHref>
-                    <a href='' className='p-4 px-8 font-regular text-m text-black bg-white baseline hover:scale-105 transition'>Start now!</a>
-                </Link>
+            <div>
+              <button onClick={onOpen} className='startedBtn text-white bg-main'>Join the Grow Community!</button>
+            </div>
             </div>
         <div className="absolute md:-top-16 md:w-[500px] w-[400px] -top-24 left-0">
-            <Image src={trianglesred} />
+            {/* <Image src={trianglesred} /> */}
         </div>
         </div>
       </section>
