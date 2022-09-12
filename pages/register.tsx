b1e7b49d3c656a078fca6ecbb8572cc8da615ea4
PTBYSR <paul.emechebe@gmail.com>
2022-09-12 15:39:03 +0100

    redeployment issues fix

--- pages/register.tsx
+++ pages/register.tsx
@@ -15,7 +15,7 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { useDisclosure } from '@chakra-ui/hooks'
-const ModalContext = createContext()
+// const ModalContext = createContext()
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Box } from "@chakra-ui/react";
 import Image from "next/image";
