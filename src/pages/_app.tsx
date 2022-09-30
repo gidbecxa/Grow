906d8d777e45fb1a11ef3dd0984db65a147ddd6d
PTBYSR <paul.emechebe@gmail.com>
2022-09-30 13:26:22 +0100

    fix: vercel deployemnt error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -42,10 +42,10 @@ export default function MyApp({ Component, pageProps : { session, ...pageProps }
   const getLayout = Component.getLayout ?? ((page) => page)
 
   return getLayout(
-    // <SessionProvider session={session}>
+  //   <SessionProvider session={session}>
+  //  </SessionProvider>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
-    {/* </SessionProvider> */}
   )
 }
