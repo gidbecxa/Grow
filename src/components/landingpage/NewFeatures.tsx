9285e3c8f2c83a1b03c607a49431f85da1bdae82
gidbecxa <gidbec4yeshua@gmail.com>
2023-03-06 14:48:39 +0100

    Translated NewFeatures section

--- src/components/landingpage/NewFeatures.tsx
+++ src/components/landingpage/NewFeatures.tsx
@@ -11,7 +11,7 @@ const NewFeatures = () => {
       {/* <div className='container flex flex-col px-4 mx-auto pt-10 mb-20 space-y-12 md:space-y-0 md:flex-row'> */}
       <div className="container flex flex-col px-12 mx-auto pt-10 mb-20 space-y-12 md:space-y-0 md:flex-row">
         <div className='flex justify-center flex-col space-y-12 md:w-[40%]'>
-          <h1 className='text-main max-w-md text-4xl font-bold text-center md:text-left '>Stand out with Grow: What sets us apart</h1>
+          <h1 className='text-main max-w-md text-4xl font-bold text-center md:text-left '>Faites-vous remarquer avec Grow : Ce qui nous distingue</h1>
           {/* <p className='md:max-w-sm text-center text-main md:text-left'>Grow is the leading software developement school and community for French-speaking Africans. We are commited to helping millions of francophones with little to no coding experience become world-class certified software engineers.</p> */}
         </div>
         <div className='flex  flex-wrap lg:gap-12 md:gap-7 sm:gap-5 md:w-[60%] justify-center md:justify-end'>
@@ -22,21 +22,21 @@ const NewFeatures = () => {
                     src={workout}
                   />
                 </div> */}
-            <h2 className='font-semibold'>Unlock your potential and build a thriving career in Tech</h2>
+            <h2 className='font-semibold'>Libérez votre potentiel et bâtissez-vous une carrière florissante en Tech.</h2>
             <p className='text-xs'>
-              Looking to kickstart your career as a software engineer? Our bootcamps offer a comprehensive learning experience that equips you with the skills and knowledge you need to succeed in tech.
+              Nos formations accélérées offrent une expérience complète qui vous permet d'acquérir les compétences et les connaissances requises pour réussir dans la Tech.
             </p>
           </div>
-          <div className='rounded-lg shadow-md w-72 h-72 p-5 flex flex-col justify-center gap-5 bg-[#C9F4FF]'>
+          <div className='rounded-lg shadow-md w-72 h-72 p-5 flex flex-col justify-center md:gap-10 bg-[#C9F4FF]'>
             {/* <div className=" bg-main rounded-full  w-10 h-10 flex justify-center items-center">
                   <Image
                     priority
                     src={rating}
                   />
                 </div> */}
-            <h2 className='font-semibold'>Expert instructors, cutting-edge programs, and hands-on projects</h2>
+            <h2 className='font-semibold'>Instructeurs experts, programmes de pointe et projets pratiques.</h2>
             <p className='text-xs'>
-              With expert instructors, cutting-edge programs, and hands-on projects, we take through a transformative learning experience that prepares you for success in the fast-paced world of software development.
+              Nos instructeurs experts, des programmes de pointe et des projets pratiques vous offrent une expérience pédagogique transformatrice qui vous prépare à une brillante carrière.
             </p>
           </div>
           <div className='rounded-lg shadow-md w-72 h-72 p-5 flex flex-col justify-center gap-5 bg-[#A6C1C7]'>
@@ -46,9 +46,9 @@ const NewFeatures = () => {
                     src={suitcase}
                   />
                 </div> */}
-            <h2 className='font-semibold'>Career support beyond the bootcamp</h2>
+            <h2 className='font-semibold'>Soutien professionnel au-delà du bootcamp</h2>
             <p className='text-xs'>
-              Get access to career guidance, job placement, and mentorship opportunities to help you launch a successful career in tech. We're committed to your long-term success.
+            Accédez à des orientations de carrière, à des offres d'emploi et à des occasions de mentorat pour vous aider à lancer une carrière réussie. Nous nous engageons à assurer votre réussite à long terme.
             </p>
           </div>
           <div className='rounded-lg shadow-md w-72 h-72 p-5 flex flex-col justify-center gap-5 bg-[#A5B2B5]'>
@@ -58,10 +58,9 @@ const NewFeatures = () => {
                     src={community}
                   />
                 </div> */}
-            <h2 className='font-semibold'>We are building a community</h2>
+            <h2 className='font-semibold'>Nous bâtissons une communauté</h2>
             <p className='text-xs'>
-              What&apos;s more exciting than a community of developers? You have a question?
-              Get support and learn from others. Share your work, build, collaborate with fellows. And yet, enjoy socializing!
+            Qu'y a-t-il de plus intéressant qu'une communauté de développeurs ? Obtenez de l'aide et apprenez des autres. Partagez votre travail, collaborez avec les autres. Profitez de la convivialité !
             </p>
           </div>
         </div>
