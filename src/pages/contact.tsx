c83b0fb08c5effff25c20f637912a3d9eb3807ea
gidbecxa <gidbec4yeshua@gmail.com>
2023-03-07 13:55:21 +0100

    Translated Contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -72,21 +72,21 @@ const contact = () => {
 
                     <div className='md:w-1/2'>
                         <div>
-                            <p className='text-[#23464F] text-xs ml-1 font-semibold mb-1'>Contact us</p>
-                            <h1 className='text-5xl font-semibold'>Let's get in touch</h1>
+                            <p className='text-[#23464F] text-xs ml-1 font-semibold mb-1'>Contactez-nous</p>
+                            <h1 className='text-5xl font-semibold'>Entrons en contact</h1>
                         </div>
                         <div className='mt-10 flex flex-col gap-5'>
                             <div>
-                                <h2 className='text-2xl font-semibold mb-1'>Our Location</h2>
-                                <p>Lorem ipsum dolor sit amet consectetur</p>
+                                <h2 className='text-2xl font-semibold mb-1'>Notre site</h2>
+                                <p>Hama, Carrefour BIBE Jéricho, C/660, Maison Ibrahim, Cotonou, Bénin</p>
                             </div>
                             <div>
-                                <h2 className='text-2xl font-semibold mb-1'>Phone Number</h2>
+                                <h2 className='text-2xl font-semibold mb-1'>Numéros</h2>
                                 <p>&#40;+229&#41; 59 09 59 09, <br /> &#40;+229&#41; 96 11 83 35</p>
                                 <p>WhatsApp: &#40;+229&#41; 59 09 59 09</p>
                             </div>
                             <div>
-                                <h2 className='text-2xl font-semibold mb-1'>Email Address</h2>
+                                <h2 className='text-2xl font-semibold mb-1'>Addresse Email</h2>
                                 <p>hello@ecolegrow.tech</p>
                             </div>
                         </div>
@@ -100,20 +100,20 @@ const contact = () => {
                                     value={formData.firstname}
                                     onChange={(event) => setFormData({ ...formData, firstname: event.target.value })}
                                     className='w-full rounded-lg p-5 outline-none'
-                                    placeholder='Name' />
+                                    placeholder='Votre nom' />
                                 <input type="text"
                                     value={formData.email}
                                     onChange={(event) => setFormData({ ...formData, email: event.target.value })}
-                                    className='w-full rounded-lg p-5 outline-none' placeholder='Email' />
+                                    className='w-full rounded-lg p-5 outline-none' placeholder='Votre email' />
                                 <input type="text"
                                     value={formData.phone}
                                     onChange={(event) => setFormData({ ...formData, phone: event.target.value })}
-                                    className='w-full rounded-lg p-5 outline-none' placeholder='Your Phone' />
+                                    className='w-full rounded-lg p-5 outline-none' placeholder='Numéro' />
                                 <textarea
                                     value={ autofill ? autofill : formData.msg}
                                     onChange={(event) => setFormData({ ...formData, msg: event.target.value })}
-                                    placeholder='Your message' className='outline-none p-5 h-32 resize-none rounded-lg'></textarea>
-                                <button type='submit' className="text-white bg-[#66cee8] border-none startedBtn">Send Message</button>
+                                    placeholder='Votre message' className='outline-none p-5 h-32 resize-none rounded-lg'></textarea>
+                                <button type='submit' className="text-white bg-[#66cee8] border-none startedBtn">Envoyer</button>
                             </div>
                         </form>
                         <div className='-z-[1000] absolute -left-[6px] -top-[5px] w-32 h-32 rounded-t-3xl rounded-bl-3xl bg-[#66CEEB] '></div>
